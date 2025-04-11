ee16d78ce9aba7d12977f3e0d41c1bec85a38581
ZosiaG420 <zofger_57@o2.pl>
2025-04-11 15:09:32 +0200

    owca - 11 kwietnia, roku Pańskiego 2025, 15:09, ZG

--- src/Sheep.java
+++ src/Sheep.java
@@ -1,2 +1,26 @@
+import javafx.scene.image.ImageView;
+
 public class Sheep {
+    int x;
+    int y;
+    int mapPart;
+    ImageView image;
+    boolean zawirusowana = false;
+    //default nie ma wirusa
+
+    public Sheep(int x, int y, int mapPart) {
+        this.x = x;
+        this.y = y;
+        this.mapPart = mapPart;
+        if (zawirusowana){
+            //change image if zawirusowana
+        }
+
+    }
+
+
+
+    public static void detectPlayer(){
+
+    }
 }
