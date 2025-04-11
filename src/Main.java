918ca951de285c746302f36e4acf383253adc71d
marta <ess.marta.oleszkiewicz@gmail.com>
2025-04-11 14:46:56 +0200

    music

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class Main extends Application {
+
+    static String musicFile1 = "farming.mp3";
+    public static Media farmingMusic = new Media(new File(musicFile1).toURI().toString());
+    public static MediaPlayer mediaPlayerStarter = new MediaPlayer(farmingMusic);
+
     public static void main(String[] args) {
-
+        launch(args);
     }
-
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        mediaPlayerStarter.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayerStarter.play();
     }
 }
