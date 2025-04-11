3364a2d1fb58ef846e07d1b4e1fc6131fb4bbf89
Daria <cortisol.the.stres5er@gmail.com>
2025-04-11 14:50:23 +0200

    Hiii, Daria here
    The first part for the Player code is being commited:)
    
    Cheers<3,
    Daria

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,16 @@
-public class Player {
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Player extends ImageView {
+
+    int x;
+    int y;
+    boolean isDown;
+
+    public Player(Image imageView) {
+        super(imageView);
+        this.x = x;
+        this.y = y;
+        this.isDown = isDown;
+    }
 }
