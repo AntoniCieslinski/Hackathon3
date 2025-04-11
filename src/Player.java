98fbc147e63b877774f39ca6ac6d0df54c1d96bb
Daria <cortisol.the.stres5er@gmail.com>
2025-04-11 16:18:11 +0200

    Hiii, Daria here
    One more part for player ig
    
    Cheers<3,
    Daria

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,8 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 public class Player extends ImageView {
 
@@ -7,10 +10,23 @@ public class Player extends ImageView {
     int y;
     boolean isDown;
 
-    public Player(Image imageView) {
+    public Player(Image imageView, int x, int y) {
         super(imageView);
         this.x = x;
         this.y = y;
-        this.isDown = isDown;
+
     }
+
+    Timeline timelinePlayer = new Timeline(new KeyFrame(Duration.millis(10), event -> {
+
+
+    }));
+
+
+
+    public boolean isDown() {
+        return isDown;
+    }
+
+
 }
